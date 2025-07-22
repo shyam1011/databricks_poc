9d565845dd59fd9eb3af10bb3f0d28ec39d66b23
Shyam Anadkat <shyam.anadkat@pepsico.com>
2025-07-22 23:25:17 +0530

    test_23

--- deploy_job.py
+++ deploy_job.py
@@ -1,9 +1,7 @@
 from databricks.sdk import WorkspaceClient
 import os
 
-
 server_hostname = os.getenv("DATABRICKS_HOST")
-http_path = os.getenv("DATABRICKS_HTTP_PATH")
 access_token = os.getenv("DATABRICKS_TOKEN")
 
 client = WorkspaceClient(
@@ -11,10 +9,20 @@ client = WorkspaceClient(
     token=access_token
 )
 
-
-
 job_settings = {
     "name": "DataLoader Serverless Job",
+    "job_clusters": [
+        {
+            "job_cluster_key": "serverless_cluster",
+            "new_cluster": {
+                "spark_version": "14.0.x-scala2.12",
+                "node_type_id": "Standard_DS3_v2",
+                "num_workers": 1,
+                "data_security_mode": "SINGLE_USER",
+                "runtime_engine": "PHOTON"
+            }
+        }
+    ],
     "environments": [
         {
             "environment_key": "default_env",
@@ -34,6 +42,7 @@ job_settings = {
     "tasks": [
         {
             "task_key": "load_data_task",
+            "job_cluster_key": "serverless_cluster",
             "python_wheel_task": {
                 "package_name": "dataloader",
                 "entry_point": "load_data"
@@ -44,8 +53,8 @@ job_settings = {
 }
 
 created_job = client.api_client.do(
-    "POST",
-    "/api/2.1/jobs/create",
+    method="POST",
+    path="/api/2.1/jobs/create",
     body=job_settings
 )
 
@@ -53,4 +62,3 @@ print(f"Created Job ID: {created_job['job_id']}")
 
 with open("job_id.txt", "w") as f:
     f.write(str(created_job['job_id']))
-
