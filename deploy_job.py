75670abaafa9499a1042dc6e68461b0c52765b18
Shyam Anadkat <shyam.anadkat@pepsico.com>
2025-07-22 23:26:57 +0530

    test_24

--- deploy_job.py
+++ deploy_job.py
@@ -35,7 +35,7 @@ job_settings = {
                         "dbfs:/FileStore/whl_poc/extract_work-0.1-py3-none-any.whl"
                     ]
                 },
-                "environment_version": "14.0.x-photon-scala2.12"
+                "client_version": "1.0.0"
             }
         }
     ],
