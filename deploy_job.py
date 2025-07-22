92f8a79aa8aa0ef1131c90bbe86c8d8446cdb762
Shyam Anadkat <shyam.anadkat@pepsico.com>
2025-07-22 22:36:08 +0530

    test_04

--- deploy_job.py
+++ deploy_job.py
@@ -1,6 +1,6 @@
 from databricks.sdk import WorkspaceClient
 import os
-from databricks.sdk.service.jobs import JobSettings
+from databricks.sdk.service.jobs import JobSettings, JobCluster, NewCluster, Task, PythonWheelTask, Library
 
 server_hostname = os.getenv("DATABRICKS_HOST")
 http_path = os.getenv("DATABRICKS_HTTP_PATH")
@@ -12,39 +12,36 @@ client = WorkspaceClient(
 )
 
 
-job_settings = {
-    "name": "DataLoader Job",
-    "job_clusters": [
-        {
-            "job_cluster_key": "serverless_cluster",
-            "new_cluster": {
-                "spark_version": "13.3.x-scala2.12",  # Adjust version
-                "num_workers": 1,
-                "data_security_mode": "SINGLE_USER",
-                "runtime_engine": "PHOTON",
-                "node_type_id": "Serverless",   # This is the serverless identifier
-                "custom_tags": {
-                    "ClusterType": "Serverless"
-                }
-            }
-        }
-    ],
-    "tasks": [
-        {
-            "task_key": "load_data_task",
-            "job_cluster_key": "serverless_cluster",
-            "python_wheel_task": {
-                "package_name": "dataloader",
-                "entry_point": "load_data"
-            },
-            "libraries": [
-                {
-                    "whl": "dbfs:/FileStore/whl_poc/extract_work-0.1-py3-none-any.whl"
-                }
-            ]
-        }
+job_cluster = JobCluster(
+    job_cluster_key="serverless_cluster",
+    new_cluster=NewCluster(
+        spark_version="13.3.x-scala2.12",
+        node_type_id="Serverless",
+        num_workers=1,
+        data_security_mode="SINGLE_USER",
+        runtime_engine="PHOTON"
+    )
+)
+
+task = Task(
+    task_key="load_data_task",
+    job_cluster_key="serverless_cluster",
+    python_wheel_task=PythonWheelTask(
+        package_name="dataloader",
+        entry_point="load_data"
+    ),
+    libraries=[
+        Library(
+            whl="dbfs:/FileStore/whl_poc/extract_work-0.1-py3-none-any.whl"
+        )
     ]
-}
+)
+
+job_settings = JobSettings(
+    name="DataLoader Serverless Job",
+    job_clusters=[job_cluster],
+    tasks=[task]
+)
 
 # Create or update the job
 created_job = client.jobs.create(**job_settings.as_dict())
