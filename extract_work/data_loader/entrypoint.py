d638f43078578e10c0d929333c724c2223d18ec4
anjali.verma <anjali.verma@pepsico.com>
2025-07-22 20:24:07 +0530

    test07222024

---
+++ extract_work/data_loader/entrypoint.py
@@ -0,0 +1,5 @@
+import data_loader
+
+if __name__ == "__main__":
+    print("Starting Databricks Jobs")
+    data_loader.main()
--- extract_work/data_loader/main.py
+++ extract_work/data_loader/main.py
@@ -39,5 +39,4 @@ def main():
         except:
             pass
 
-if __name__ == "__main__":
-    main()
+
