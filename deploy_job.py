5d780c07333a03495defaaa1db7e11fb8d51ec89
Shyam Anadkat <shyam.anadkat@pepsico.com>
2025-07-22 22:51:21 +0530

    test_11

--- deploy_job.py
+++ deploy_job.py
@@ -1,6 +1,6 @@
 from databricks.sdk import WorkspaceClient
 import os
-from databricks.sdk.service.jobs import JobSettings,JobSettings, JobCluster, Task, PythonWheelTask
+from databricks.sdk.service.jobs import JobSettings, JobCluster, Task, PythonWheelTask
 
 server_hostname = os.getenv("DATABRICKS_HOST")
 http_path = os.getenv("DATABRICKS_HTTP_PATH")
@@ -12,46 +12,40 @@ client = WorkspaceClient(
 )
 
 
-job_cluster = JobCluster(
-    job_cluster_key="serverless_cluster",
-    new_cluster={
-        "spark_version": "13.3.x-scala2.12",
-        "node_type_id": "Serverless",
-        "num_workers": 1,
-        "data_security_mode": "SINGLE_USER",
-        "runtime_engine": "PHOTON"
-    }
-)
-
-task = Task(
-    task_key="load_data_task",
-    job_cluster_key="serverless_cluster",
-    python_wheel_task=PythonWheelTask(
-        package_name="dataloader",
-        entry_point="load_data"
-    ),
-    libraries=[
+job_settings = {
+    "name": "DataLoader Serverless Job",
+    "job_clusters": [
         {
-            "whl": "dbfs:/FileStore/whl_poc/extract_work-0.1-py3-none-any.whl"
+            "job_cluster_key": "serverless_cluster",
+            "new_cluster": {
+                "spark_version": "13.3.x-scala2.12",
+                "node_type_id": "Serverless",
+                "num_workers": 1,
+                "data_security_mode": "SINGLE_USER",
+                "runtime_engine": "PHOTON"
+            }
+        }
+    ],
+    "tasks": [
+        {
+            "task_key": "load_data_task",
+            "job_cluster_key": "serverless_cluster",
+            "python_wheel_task": {
+                "package_name": "dataloader",
+                "entry_point": "load_data"
+            },
+            "libraries": [
+                {
+                    "whl": "dbfs:/FileStore/whl_poc/extract_work-0.1-py3-none-any.whl"
+                }
+            ]
         }
     ]
-)
+}
 
-job_settings = JobSettings(
-    name="DataLoader Serverless Job",
-    job_clusters=[job_cluster],
-    tasks=[task]
-)
-
-# Create or update the job
-created_job = client.jobs.create(
-    name=job_settings.name,
-    job_clusters=[jc.as_dict() for jc in job_settings.job_clusters],
-    tasks=[t.as_dict() for t in job_settings.tasks]
-)
+created_job = client.jobs.create(**job_settings)
 print(f"Created Job ID: {created_job.job_id}")
 
-
-# Save job ID to file
 with open("job_id.txt", "w") as f:
     f.write(str(created_job.job_id))
+
